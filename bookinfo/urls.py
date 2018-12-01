1d899062824530af5a15694f07c3a43597e16b23
Saifur <saifurrahmany43@gmail.com>
2018-12-01 20:32:47 +0600

    search

--- bookinfo/urls.py
+++ bookinfo/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.BookListView.as_view(),name='home'),
     re_path(r'^(?P<id>\d+)/(?P<slug>[-\w\d]+)/$',views.BookDetailView.as_view(),name='details'),
     re_path(r'^categories/(?P<c_slug>[-\w\d]+)/$',views.CategoryListView.as_view(),name='category'),
+    re_path(r'^search/$', views.BookSearch.as_view(), name='search'),
+    
 ]                                                
--- bookinfo/views.py
+++ bookinfo/views.py
@@ -1,22 +1,68 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Book
+from django.views.generic import ListView, DetailView, View
+from .models import Book, Category
+
 
 class BookListView(ListView):
     model = Book
-    context_object_name = 'books'
     queryset = Book.objects.all()
     template_name = 'bookinfo/home.html'
-    
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context_data = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        books = Book.objects.all()
+        categories = Category.objects.all()
+        context_data['books'] = books
+        context_data['categories'] = categories
+
+        return context_data
+
+
 class BookDetailView(DetailView):
     model = Book
     pk_url_kwarg = "id"
     template_name = 'bookinfo/details.html'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context_data = super(BookDetailView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        categories = Category.objects.all()
+        context_data['categories'] = categories
+
+        return context_data
+
+
 class CategoryListView(ListView):
     model = Book
     template_name = 'bookinfo/category.html'
     context_object_name = 'books'
+
     def get_queryset(self):
         if self.kwargs['c_slug']:
             return self.model.objects.filter(categories__c_slug=self.kwargs['c_slug'])
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context_data = super(CategoryListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        categories = Category.objects.all()
+        context_data['categories'] = categories
+
+        return context_data
+
+
+class BookSearch(View):
+
+    def post(self, request):
+        search_text = request.POST.get('search')
+        categories = []
+        if search_text:
+            books = Book.objects.filter(
+                title__icontains=search_text).distinct()
+        else:
+            books = ['No Items found']
+
+        return render(request, 'bookinfo/search.html', {'books': books, 'categories': categories})
